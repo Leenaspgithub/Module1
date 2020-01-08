bcfe629594aa0a6f1ec2152a9178dc70b18dea89
LEENA PATIL <41008714@adani.com>
2020-01-08 12:23:17 +0530

    Final submission of Module 1 assignment

--- app.js
+++ app.js
@@ -5,13 +5,24 @@
 LunchCheckController.$inject=['$scope'];
 
 function LunchCheckController($scope){
-  $scope.name="Leena";
-  $scope.stateOfBeing="sad";
-  $scope.sayMessage=function(){
-    return " likes the AngularJS, I am Happy!";
-  };
-  $scope.makeHappy=function(){
-    $scope.stateOfBeing="happy";
+  $scope.name="";
+  $scope.defaultmessage="";
+  $scope.checkIfToomuch=function(){
+const arrayOfStrings = $scope.name.split(',');
+console.log('The original string is: ',$scope.name)
+  console.log('The separator is: ', ',')
+  console.log('The array has ', arrayOfStrings.length, ' elements: ', arrayOfStrings.join(' / '))
+
+  if($scope.name==""){
+    $scope.defaultmessage="Please enter data first";
+      }
+else if(arrayOfStrings.length<3){
+    $scope.defaultmessage="Enjoy!";
+  }
+else  if(arrayOfStrings.length>3){
+    $scope.defaultmessage="Too Much!";
+  }
+  return   $scope.defaultmessage;
   };
 }
 
